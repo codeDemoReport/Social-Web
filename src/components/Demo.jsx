16379f872bafa213ecdd4855d8229f56a7ea51bd
Tran Van Luc <luctv_tts@rikkeisoft.com>
2022-04-15 09:17:29 +0700

    first commit

---
+++ src/components/Demo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Demo(props) {
+  return <div>demo</div>;
+}
+
+export default Demo;
---
+++ src/pages/Demo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Demo(props) {
+  return <div>Demo</div>;
+}
+
+export default Demo;
